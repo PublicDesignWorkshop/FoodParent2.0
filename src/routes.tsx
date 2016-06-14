dfc286923a7ce67562970f6b92d2e94a4b8128d7
Karl Kim <captainwhale52@gmail.com>
2016-06-14 10:13:34 -0400

    Check the git commit.

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import * as Router from 'react-router';
 import { Route, IndexRoute } from 'react-router';
 
 var Settings = require('./constraints/settings.json');
+
 import NoMatchComponent from './components/nomatch.component';
 import AppComponent from './components/app.component';
 import TreesComponent from './components/trees/trees.component';
 import DonationsComponent from './components/donations/donations.component';
 import LoginComponent from './components/parent/login.component';
+
 
 var RouteMap = (
     <Route path={Settings.uBaseNameForWebPack} component={AppComponent}>
