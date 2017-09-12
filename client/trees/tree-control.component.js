768008c9281a9b86785ba0ee3d7cf4596aa88d16
Karl <captainwhale52@gmail.com>
2017-09-12 06:22:33 -0500

    <feat>: Add intro-js with an example code

--- client/trees/tree-control.component.js
+++ client/trees/tree-control.component.js
@@ -15,15 +15,23 @@ let TreeStore = require('./../stores/tree.store');
 import { MAPTILE, MAPTYPE } from './../utils/enum';
 import { localization } from './../utils/localization';
 
+import { Steps } from 'intro.js-react';
+
+require('./../intro.scss');
+
 
 export default class TreeControl extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.handleToggleMapTile = this.handleToggleMapTile.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
+    this.onExit = this.onExit.bind(this);
   }
   componentWillMount() {
-    this.setState({tile: "map-o"});
+    this.setState({
+      tile: "map-o",
+      tutorial: false
+    });
   }
   componentDidMount () {
   }
@@ -63,20 +71,97 @@ export default class TreeControl extends React.Component {
     //   // this.context.router.push({pathname: ServerSetting.uBase + '/tree/' + parseInt(searchText)});
     // }
   }
+  onExit() {
+    this.setState({
+      tutorial: false
+    })
+  }
   render () {
-    let add = <div className="control-button" onClick={()=> {
+    let steps = [
+      {
+        element: '.center',
+        intro: 'Search your location.',
+        position: 'bottom',
+        tooltipClass: 'introjs-searchbar',
+        // highlightClass: 'myHighlightClass',
+      },
+      {
+        element: '.right',
+        intro: 'Become a food parent.',
+        position: 'bottom',
+      },
+      {
+        element: '.intro-location',
+        intro: 'Find your location.',
+        position: 'left',
+      },
+      {
+        element: '.intro-filter',
+        intro: 'Select food type.',
+        position: 'left',
+      },
+      {
+        element: '.intro-add',
+        intro: 'Add a new food.',
+        position: 'left',
+      },
+    ];
+
+    if (this.props.adding) {
+      steps = [
+        {
+          element: '.center',
+          intro: 'Search your location.',
+          position: 'bottom',
+          tooltipClass: 'introjs-searchbar',
+          highlightClass: 'myHighlightClass',
+        },
+        {
+          element: '.right',
+          intro: 'Become a food parent.',
+          position: 'bottom',
+        },
+        {
+          element: '.intro-location',
+          intro: 'Find your location.',
+          position: 'left',
+        },
+        {
+          element: '.intro-filter',
+          intro: 'Select food type.',
+          position: 'left',
+        },
+        {
+          element: '.intro-add',
+          intro: 'Cancel adding a new food.',
+          position: 'left',
+        }
+      ];
+    }
+
+    const intro = <Steps
+      enabled={this.state.tutorial}
+      steps={steps}
+      initialStep={0}
+      onExit={this.onExit}
+    />
+
+    let add = <div className="control-button intro-add" onClick={()=> {
       this.context.router.push({pathname: ServerSetting.uBase + '/addtree'});
     }} data-for="tooltip-tree-control" data-tip={localization(85)}>
       <FontAwesome name="plus-square" />
     </div>;
 
-    let help = <div className="control-button" onClick={
-    } data-for="tooltip-tree-control" data-tip={localization(89)}>
+    let help = <div className="control-button" data-for="tooltip-tree-control" data-tip={localization(89)}
+      onClick={() => {
+        this.setState({tutorial: true});
+      }}
+    >
       <FontAwesome name="question" />
     </div>;
 
     if (this.props.adding) {
-      add = <div className="control-button" onClick={()=> {
+      add = <div className="control-button intro-add" onClick={()=> {
         TreeActions.setCode(0); // set TreeAction code as 0 so that map component can wait rendering map after trees are updated.
         this.context.router.push({pathname: ServerSetting.uBase + '/'});
         // if (MapStore.getState().latestMapType == MAPTYPE.TREE) {
@@ -109,7 +194,7 @@ export default class TreeControl extends React.Component {
     }
     return (
       <div className="tree-control-wrapper">
-        <div className="control-button" onClick={this.handleMoveToUserLocation} data-for="tooltip-tree-control" data-tip={localization(80)}>
+        <div className="control-button intro-location" onClick={this.handleMoveToUserLocation} data-for="tooltip-tree-control" data-tip={localization(80)}>
           <FontAwesome name='location-arrow' />
         </div>
         <div className="control-button" onClick={this.handleToggleMapTile} data-for="tooltip-tree-control" data-tip={localization(81)}>
@@ -121,7 +206,7 @@ export default class TreeControl extends React.Component {
         <div className="control-button" onClick={this.handleZoomOut} data-for="tooltip-tree-control" data-tip={localization(83)}>
           <FontAwesome name='search-minus' />
         </div>
-        <div className="control-button" onClick={this.handleFilter} data-for="tooltip-tree-control" data-tip={emptyFilter} ref="filter">
+        <div className="control-button intro-filter" onClick={this.handleFilter} data-for="tooltip-tree-control" data-tip={emptyFilter} ref="filter">
           <FontAwesome name='sliders'/>
         </div>
         <ReactTooltip id="tooltip-tree-control" effect="solid" place="left" />
@@ -129,6 +214,7 @@ export default class TreeControl extends React.Component {
         {notify}
         {donation}
         {help}
+        {intro}
       </div>
     );
   }
