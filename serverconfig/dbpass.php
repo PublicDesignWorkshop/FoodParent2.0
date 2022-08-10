2082f0a485d30fca0df8c55ff1bbf712f28a7b87
Nater <natomato@users.noreply.github.com>
2022-08-10 14:50:40 -0500

    remove serverconfig files from git

--- serverconfig/dbpass.php
+++
@@ -1,5 +0,0 @@
-<?php
-  class dbpass {
-    public $password = "";
-  }
-?>
--- serverconfig/mailconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-  class mailconfig {
-    public $apikey = "key-example";
-    public $domain = "mail-domain";
-    public $from = "mail-sender";
-  }
-?>
